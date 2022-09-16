91d6ea91aac09f82f6afd6a8fbe295dd208a7c8e
Zeyu Mao <mao.mzy@gmail.com>
2022-09-16 11:19:07 -0500

    1. Install the latest esa

--- src/esa.jl
+++ src/esa.jl
@@ -1,5 +1,15 @@
-module esa
+module ESA
 
-# Write your package code here.
+using PyCall: PyNULL, pyimport
+
+export esa
+const esa = PyNULL()
+
+function __init__()
+    const PIP_PACKAGES = ["esa"]
+    sys = pyimport("sys")
+    subprocess = pyimport("subprocess")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", PIP_PACKAGES...])
+end
 
 end
