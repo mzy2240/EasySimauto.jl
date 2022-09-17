698489984cc37b9a39e674ae4827d651db828e04
Zeyu Mao <mao.mzy@gmail.com>
2022-09-17 10:46:13 -0500

    1. Detect JULIA_REGISTRYCI_AUTOMERGE var to avoid the automerge failure

--- src/EasySimauto.jl
+++ src/EasySimauto.jl
@@ -6,17 +6,19 @@ using Conda
 
 export esa, SAW
 
-# install conda package
-Conda.pip_interop(true)
-Conda.pip("install", "esa")
-
 # https://pypi.org/project/esa/
 const esa = PyNULL()
 const SAW = PyNULL()
 
-function __init__() 
-    copy!(esa, pyimport("esa"))
-    copy!(SAW, pyimport("esa").SAW)
+
+function __init__()
+    if get(ENV, "JULIA_REGISTRYCI_AUTOMERGE", "false") != "true"
+        # install conda package
+        Conda.pip_interop(true)
+        Conda.pip("install", "esa")
+        copy!(esa, pyimport("esa"))
+        copy!(SAW, pyimport("esa").SAW)
+    end
 end
 
 end
